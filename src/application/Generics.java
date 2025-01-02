c3247be8dd9589c1c124dba747cc3ea858ee0364
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-01-02 08:19:10 -0300

    Utilizacao do Objetct com valores inteiros

--- src/application/Generics.java
+++ src/application/Generics.java
@@ -1,10 +1,34 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import services.PrintService;
+
 public class Generics {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		PrintService ps = new PrintService();
+		
+		System.out.print("How many values? ");
+		Integer n = sc.nextInt();
+		
+		for ( int i =0; i<n; i++) {
+			Integer value = sc.nextInt();
+			ps.addValue(value);
+		}
+		
+		System.out.println();
+		System.out.print("Values:");
+		ps.print();
+		System.out.println("]");
+		System.out.println("First: " + ps.first());
+		
+		sc.close();
 	}
 
 }
